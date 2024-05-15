ef19507fe6aa6971f1782cd812dbb90f3f82a043
Waqar Amin <vacaramin86@gmail.com>
2024-05-15 05:20:03 +0500

    Navbar set

---
+++ client/script.js
@@ -0,0 +1,7 @@
+
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
